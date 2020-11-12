8f206d81f16fdffa9a9626940966ddde4366d753
Whitney0814 <sosuyong030814@gmail.com>
2020-11-12 15:18:03 +0900

    database_3_Modify

--- select.sql
+++ select.sql
@@ -292,3 +292,44 @@ WHERE
 -- -> 테이블에 CATEGORY ->  제약조건에 CATEGORY_PK -> 로컬 열에 CATEGORY_ID
 -- Model 탭에서 확인
 
+-- COUNT, SUM, AVG, MIN, MAX
+--  개수, 합, 평균, 최소, 최대
+-- 예) SELECT SUM(PRICE) FROM PRODUCT;
+
+-- 문제 14.
+-- 모든 상품의 개수, 가격의 합, 평균, 최대, 최소값을 동시에 뽑아주세요
+
+SELECT 
+    CATEGORY_ID,
+    COUNT(PRICE) AS 개수,
+    SUM(PRICE) AS 가격의_합,
+    AVG(PRICE) AS 가격의_평균,
+    MAX(PRICE) AS 가격의_최대,
+    MIN(PRICE) AS 가격의_최소
+FROM
+    PRODUCT
+GROUP BY
+    CATEGORY_ID
+HAVING 
+    SUM(PRICE) < 10000000;
+
+-- 구문의 순서
+-- SELECT, FROM, WHERE, GROUP BY, HAVING, ORDER BY
+-- 문제 15.
+-- 모든 상품의 개수, 가격의 합, 가격의 평균, 가격의 최대, 가격의 최소값을 동시에 뽑아주세요
+-- 카테고리 별로 그룹핑하여 보여주세요
+-- 다만, 그룹핑 하기 전의 상품의 가격이 1,000,000 넘는 상품은 제외해주세요
+
+SELECT 
+    CATEGORY_ID,
+    COUNT(PRICE) AS 개수,
+    SUM(PRICE) AS 가격의_합,
+    AVG(PRICE) AS 가격의_평균,
+    MAX(PRICE) AS 가격의_최대,
+    MIN(PRICE) AS 가격의_최소
+FROM
+    PRODUCT
+WHERE
+    PRICE <= 1000000
+GROUP BY
+    CATEGORY_ID;
